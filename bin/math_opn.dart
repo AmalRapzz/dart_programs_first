dcfe878d16fca920c8c12e47b005f9290edea69a
AmalRapzz <amalrappz2002@gmail.com>
2023-05-17 12:16:06 +0530

    commit

---
+++ bin/math_opn.dart
@@ -0,0 +1,40 @@
+void add(){
+  int a=20,b=10;
+  print("sum = ${a+b}");
+}
+
+void sub(){
+  int a=20,b=10;
+  print("diff = ${a-b}");
+}
+
+void mul(){
+  int a=20,b=10;
+  print("Mul = ${a*b}");
+}
+
+void div(){
+  int a=20,b=10;
+  print("div = ${a/b}");
+}
+
+void main(){
+print("");
+
+  print ('a = 20 & b = 10');
+
+print("");
+
+  add();
+  sub();
+  mul();
+  div();
+
+}
+
+///Functions ---It is the Set of Statements to perform particular task or operation.
+///
+///               ---Main Categories of function ---
+///
+///           1. Built-In Functions   [eg: main function]
+///    2.User Defined Functions [eg: sum(),sub(),mul(),div(),etc..]
--- bin/sample_sum.dart
+++ bin/sample_sum.dart
@@ -10,12 +10,13 @@ class sum{
      print(a+b+c);
    }
 
+   //print(a+b);  //This will show an error since print function cannot be accessed inside the class it should be inside function.
 }
 
 void main() {
 
   sum obj = sum();
-  obj.add();      //function calling  -> Object_Name.Function_Name();  [ --if the function is inside the class-- ]
+  obj.add();      //function calling  -> Object_Name.Function_Name();  [ --if the function is inside the class object is needed to call that function-- ]
   //print(obj.a+obj.b);
   //print (c);   // This will show an error ,since it is not defined inside main function
   //print (obj.c);  // And this also show an error , because c is not a instance variable.
